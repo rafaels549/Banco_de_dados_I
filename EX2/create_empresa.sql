f0ec0d792e9c8e6ff8e38e2cbf9f14dba2ebac7e
Rafael Santos Costa <88844916+rafaels549@users.noreply.github.com>
2024-04-23 20:02:53 -0300

    Update create_empresa.sql

--- EX2/create_empresa.sql
+++ EX2/create_empresa.sql
@@ -2,6 +2,6 @@ create table empresa(
        numero int primary key generated always as identity,
        
        nome char(30) not null,
-       endereco references endereco(cep) not null,
+       endereco references endereco(id) not null,
        gerente char(40) not null,
     );
